af34b19e4c0a2f500848799f9cebd7513f5e961a
Documenter.jl <documenter@juliadocs.github.io>
2025-11-05 11:59:23 +0000

    build based on 871f93d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AllometricModels","page":"Home","title":"AllometricModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AllometricModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
